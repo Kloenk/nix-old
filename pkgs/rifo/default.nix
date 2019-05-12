7bd975aa8d4081162205c7ffe7989aa9ed8e89c0
Kloenk <kloenk@kloenk.de>
2019-05-12 22:09:28 +0200

    Merge content from PR 1

--- pkgs/rifo/default.nix
+++ pkgs/rifo/default.nix
@@ -1,6 +1,18 @@
-{ stdenv, fzf, jq, pass, termite, fetchgit, xdotool, oathToolkit }:
+with lib;
 
-stdenv.mkDerivation rec {
+ let
+  dependencies = [
+    fzf
+    termite
+    jq
+    pass
+    termite
+    oathToolkit
+    xdotool
+    bash
+  ];
+
+ in stdenv.mkDerivation {
   name = "rifo";
   version = "0.2.1_r1";
 
@@ -10,11 +22,23 @@ stdenv.mkDerivation rec {
     sha256 = "0czgssv5m57b995xv2558rqb7ym5cc2rvx57z9ag0a91a57vv5f6";
   };
 
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
   installPhase = ''
     mkdir -p $out/bin
     install -Dm0755 rifopass.sh $out/bin/rifopass
     install -Dm0755 rifo.sh $out/bin/rifo
     install -Dm0755 passlistgen.sh $out/bin/passlistgen.sh
     install -Dm0755 rifo-init.sh $out/bin/rifo-init.sh
+    wrapProgram "$out/bin/rifopass" --set PATH:${makeBinPath dependencies}
+    wrapProgram "$out/bin/rifo" --set PATH:${makeBinPath dependencies}
+    wrapProgram "$out/bin/passlistgen.sh" --set PATH:${makeBinPath dependencies}
+    wrapProgram "$out/bin/rifo-init.sh" --set PATH:${makeBinPath dependencies}
   '';
+
+  meta = {
+    license = licenses.gpl3;
+  };
 }
