1a4142d81527eb2b8c428ec703f3321b0a1774e1
Kloenk <kloenk@kloenk.de>
2019-06-04 22:25:15 +0200

    add pytradfri

--- configuration/server/home-assistant.nix
+++ configuration/server/home-assistant.nix
@@ -1,12 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
 
     services.home-assistant.enable = true;
     services.home-assistant.package = pkgs.home-assistant.override {
-        extraPackages = ps: with ps; [ colorlog ];
-        skipPip = false;
-    }
+        extraPackages = ps: with ps; [ colorlog pytradfri ];
+    };
     services.home-assistant.configWritable = true;
 
     networking.firewall.allowedTCPPorts = [ 8123 ];
