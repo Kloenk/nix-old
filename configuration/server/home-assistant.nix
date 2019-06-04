009f46e862d1d847b090f401e656e640ec7e1bdc
Kloenk <kloenk@kloenk.de>
2019-06-04 22:11:32 +0200

    update package

--- configuration/server/home-assistant.nix
+++ configuration/server/home-assistant.nix
@@ -3,6 +3,10 @@
 {
 
     services.home-assistant.enable = true;
+    services.home-assistant.package = pkgs.home-assistant.override {
+        extraPackages = ps: with ps; [ colorlog ];
+        skipPip = false;
+    }
     services.home-assistant.configWritable = true;
 
     networking.firewall.allowedTCPPorts = [ 8123 ];
