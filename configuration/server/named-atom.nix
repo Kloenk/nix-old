a2210558d24e1db8b583d4ab3aa939840a97db75
Kloenk <kloenk@kloenk.de>
2019-06-08 19:06:00 +0200

    import blocklist

--- configuration/server/named-atom.nix
+++ configuration/server/named-atom.nix
@@ -2,33 +2,8 @@
 
 
 let 
-    rpz-zone = pkgs.writeText "rpz.zone" ''
-$ORIGIN rpz.
-$TTL 1H
-@       IN       SOA       atom.kloenk.de. atom.fritz.box. (
-                           7
-                           1H
-                           15m
-                           30d
-                           2h )
-                           NS LOCALHOST.
-
-www.yahoo.com       CNAME   .
-weather.yahoo.com   CNAME   *.
-stocks.yahoo.com    CNAME   .
-ad.yahoo.com        CNAME   .
-
-ads.kelbyone.com    CNAME   .
-adsniper.ru         CNAME   .
-adsponse.de         CNAME   .
-
-displayads-formats.googleusercontent.com    CNAME   .
-tpc.googlesyndication.com   CNAME   .
-static.googleadsserving.cn  CNAME   .
-googleads.g.doubleclick.net CNAME   .
-blogergadgets.googlecode.com    CNAME   .
-googleads4.g.doubleclick.net    CNAME   .
-    '';
+    rpz-zone = pkgs.writeText "rpz.zone" (import ./common/named-blocklist.nix { inherit pkgs; });
+    
 in {
     imports = [
         ./common/named-common.nix
