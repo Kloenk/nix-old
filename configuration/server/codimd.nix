138f11a3950e980cd835e4dc8016ff5d927906f7
Kloenk <kloenk@kloenk.de>
2019-07-16 16:43:33 +0200

    fix codi

--- configuration/server/codimd.nix
+++ configuration/server/codimd.nix
@@ -3,7 +3,7 @@
 {
     services.codimd = {
         enable = true;
-        services.codimd.workDir = /data/codimd;
+        workDir = /data/codimd;
         configuration.port = 3001;
         configuration.path = "/var/run/codimd.sock";
         configuration.domain = "codi.kloenk.de";
