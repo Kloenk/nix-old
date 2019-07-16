8537a0ada32a9bfc36d41254cd2e264c275da55e
Kloenk <kloenk@kloenk.de>
2019-07-16 16:44:47 +0200

    add codimd db

--- configuration/server/codimd.nix
+++ configuration/server/codimd.nix
@@ -7,5 +7,9 @@
         configuration.port = 3001;
         configuration.path = "/var/run/codimd.sock";
         configuration.domain = "codi.kloenk.de";
+        configuration.db = {
+            dialect = "sqlite";
+            storage = "/data/codimd/db.codimd.sqlite";
+        };
     };
 }
