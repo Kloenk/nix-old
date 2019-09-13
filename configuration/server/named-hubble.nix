0d2e4a10a0910fdb7586d8391dbc6c4e2799cdce
Kloenk <kloenk@kloenk.de>
2019-09-13 17:44:09 +0200

    add dns firewall open. DAFUQ????

--- configuration/server/named-hubble.nix
+++ configuration/server/named-hubble.nix
@@ -6,6 +6,11 @@
         ./common/named-common.nix
     ];
 
+    networking.firewall = {
+      allowedTCPPorts = [ 53 ];
+      allowedUDPPorts = [ 53 ];
+    };
+
     services.bind = {
         extraOptions = ''
 also-notify { 159.69.179.160; 51.254.249.185; 192.168.42.4; 51.254.249.182; 192.168.42.7; 216.218.133.2; 2001:470:600::2; 5.45.100.14; 164.132.31.112; };
