13ead9a20688b8aa72916ac1d524d4f8359f504b
Kloenk <kloenk@kloenk.de>
2019-10-09 18:33:42 +0200

    revert nat

--- configuration/hosts/hubble.nix
+++ configuration/hosts/hubble.nix
@@ -18,7 +18,6 @@ in {
 
     ../server/common/nginx-common.nix
     ../server/wireguard.nix
-    ../server/nat.nix
     ../server/named-hubble.nix
     ../server/docker.nix
     #../server/gitlab.nix
