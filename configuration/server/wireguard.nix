d49594b9a09420b0cb7baede92dd80e5a63d4e97
Kloenk <kloenk@kloenk.de>
2019-10-19 18:27:09 +0200

    add vepi to wireguard

--- configuration/server/wireguard.nix
+++ configuration/server/wireguard.nix
@@ -116,6 +116,12 @@
           presharedKeyFile = "/etc/nixos/secrets/llg0.mobile.psk";
           persistentKeepalive = 21;
         }
+	{ # vepi
+	  publicKey = "a7rQr9woO8tXBf0jfPF4F39zDgskoEJJGCbkGCcgKTo=";
+	  allowedIPs = [ "192.168.43.240", "2001:41d0:1004:1629:1337:187:43:240/128" ];
+          presharedKeyFile = "/etc/nixos/secrets/llg0.vepi.psk";
+          persistentKeepalive = 21;
+	}
       ];
     };
   };
