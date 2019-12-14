68c48438622da67b94adbd1a9b7f6836f97106fc
Kloenk <klshellInitoenk@kloenk.de>
2019-12-14 09:59:08 +0100

    add docker0 whitlist in firewall

--- configuration/server/docker.nix
+++ configuration/server/docker.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
 
 {
-      virtualisation.docker.enable = true;
-      users.users.kloenk.extraGroups = [ "docker" ];
-      users.users.kloenk.packages = [ pkgs.docker ];
+  networking.firewall.interface."docker0" = {
+    allowedTCPPortRanges = [ { from = 1; to = 65534; } ];
+    allowedUDPPortRanges = [ { from = 1; to = 65534; } ];
+  };
+
+  virtualisation.docker.enable = true;
+  users.users.kloenk.extraGroups = [ "docker" ];
+  users.users.kloenk.packages = [ pkgs.docker ];
 }
+
